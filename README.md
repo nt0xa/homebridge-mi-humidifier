817770ad8b6a3b328a3fe4bb976aa34092501f33
azrael <azrael.25.91@gmail.com>
2019-08-04 23:06:16 +0300

    update schema; a lot of refactoring

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 A Xiaomi Mi humidifier plugin for Homebridge.
 
-## Example config
+### Example config
 
 ```json
 {
@@ -23,9 +23,22 @@ A Xiaomi Mi humidifier plugin for Homebridge.
       "ip": "192.168.x.x",
       "token": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
       "model": "v1",
-      "showTemperature": true,
-      "nameTemperature": "Bedroom Temperature 1"
+      "temperature": {
+        "name": "Bedroom Temperature 1"
+      },
+      "humidity": true
     }
   ]
 }
 ```
+
+### Humidifier configuration
+
+- `ip` – the device IP address;
+- `token` – the device token (32 hex chars);
+- `model` – the model of the humidifier. Either `v1` for Smartmi Humidifier or `ca1` for Smartmi Evaporative Humidifier;
+- `name` (optional) – the name of the device. Defaults to 'Humidifier';
+- `temperature` (optional) – if defined, the temperature sensor will be added. Can be either boolean or object with properties:
+    - `name` – the name of the sensor. Defaults to 'Temperature';
+- `humidity` (optional) – if defined, the humidity sensor will be added. Can be either boolean or object with properties:
+    - `name` – the name of the sensor. Defaults to 'Humidity'.
--- index.js
+++ index.js
@@ -1,285 +1,256 @@
-const miio = require('miio')
+const miio = require('miio');
 
-let Service, Characteristic
+const defaults = {
+    name: 'Humidifier',
+    temperature: false,
+    humidity: false
+};
+
+// Note: the `auto` mode can be set only in the Smartmi Evaporative Humidifier (zhimi.humidifier.ca1)
+const speedLevels = ['off', 'silent', 'medium', 'high', 'auto'];
+
+let Service, Characteristic;
 
 module.exports = homebridge => {
-  Service = homebridge.hap.Service
-  Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-mi-humidifier', 'MiHumidifier', MiHumidifier)
-}
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-mi-humidifier', 'MiHumidifier', MiHumidifier);
+};
 
 class MiHumidifier {
+    constructor(log, config) {
+        if (!config.ip) throw new Error('Your must provide IP address of the Humidifier');
+        if (!config.token) throw new Error('Your must provide token of the Humidifier');
+        if (!config.model) throw new Error('Your must provide model of the Humidifier (v1 or ca1)');
 
-  constructor(log, config) {
-    this.log = log
-    this.ip = config.ip
-    this.token = config.token
-    this.name = config.name || 'Humidifier'
-    this.model = config.model || 'v1'
-    this.showTemperature = config.showTemperature || false
-    this.showHumidity = config.showHumidity || false
-    this.nameTemperature = config.nameTemperature || 'Temperature'
-    this.nameHumidity = config.nameHumidity || 'Humidity'
+        let options = { ...defaults, ...config },
+            info = new Service.AccessoryInformation(),
+            device = new Service.HumidifierDehumidifier(options.name);
 
-    this.services = []
+        this.log = log;
+        this.ip = config.ip;
+        this.token = config.token;
+        this.services = [device, info];
 
-    if (!this.ip)
-      throw new Error('Your must provide IP address of the Humidifier.')
+        // Device info
+        info
+            .setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
+            .setCharacteristic(Characteristic.Model, 'Humidifier')
+            .setCharacteristic(Characteristic.SerialNumber, 'Undefined');
 
-    if (!this.token)
-      throw new Error('Your must provide token of the Humidifier.')
+        // Active
+        device
+            .getCharacteristic(Characteristic.Active)
+            .on('get', this.getActive.bind(this))
+            .on('set', this.setActive.bind(this));
 
-    // Create service
-    this.service = new Service.HumidifierDehumidifier(this.name)
+        // Current state
+        device
+            .getCharacteristic(Characteristic.CurrentHumidifierDehumidifierState)
+            .on('get', this.getCurrentHumidifierState.bind(this));
 
-    // Active
-    this.service
-      .getCharacteristic(Characteristic.Active)
-      .on('get', this.getActive.bind(this))
-      .on('set', this.setActive.bind(this))
+        // Target state (only humidifier is supported)
+        device
+            .getCharacteristic(Characteristic.TargetHumidifierDehumidifierState)
+            .setValue(Characteristic.TargetHumidifierDehumidifierState.HUMIDIFIER);
 
-    // Current state
-    this.service
-      .getCharacteristic(Characteristic.CurrentHumidifierDehumidifierState)
-      .on('get', this.getCurrentHumidifierState.bind(this))
+        // Current relative humidity
+        device
+            .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+            .on('get', this.getCurrentRelativeHumidity.bind(this));
 
-    // Target state (only humidifier is supported)
-    this.service
-      .getCharacteristic(Characteristic.TargetHumidifierDehumidifierState)
-      .setValue(Characteristic.TargetHumidifierDehumidifierState.HUMIDIFIER)
+        // Target relative humidity
+        // Note: this Characteristic cannot be viewed in the Home.app, but it can be changed using Siri Voice Commands or by using some 3rd Party HomeKit apps
+        device
+            .addCharacteristic(Characteristic.TargetRelativeHumidity)
+            .on('get', this.getTargetRelativeHumidity.bind(this))
+            .on('set', this.setTargetRelativeHumidity.bind(this));
 
-    // Current relative humidity
-    this.service
-      .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-      .on('get', this.getCurrentRelativeHumidity.bind(this))
+        // Current water level (remaining water level)
+        // Note: this characteristic works only for Smartmi Evaporative Humidifier (zhimi.humidifier.ca1)
+        options.model === 'ca1' && device
+            .getCharacteristic(Characteristic.WaterLevel)
+            .on('get', this.getWaterLevel.bind(this));
 
-    // Target relative humidity
-    // This Characteristic cannot be viewed in the Home.app, but it can be changed using Siri Voice Commands or by using some 3rd Party HomeKit apps.
-    this.service
-      .addCharacteristic(Characteristic.TargetRelativeHumidity)
-      .on('get', this.getTargetRelativeHumidity.bind(this))
-      .on('set', this.setTargetRelativeHumidity.bind(this))
+        // Rotation speed
+        device
+            .getCharacteristic(Characteristic.RotationSpeed)
+            .setProps({
+                minValue: 0,
+                maxValue: options.model === 'ca1' ? 4 : 3,
+                minStep: 1
+            })
+            .on('get', this.getRotationSpeed.bind(this))
+            .on('set', this.setRotationSpeed.bind(this));
 
-    // Current water level (remaining water level)
-    // This characteristic works for zhimi.humidifier.ca1 SmartMi Evaporative Humidifier
-    if (this.model === 'ca1') {
-      this.service
-        .getCharacteristic(Characteristic.WaterLevel)
-        .on('get', this.getWaterLevel.bind(this))
-    }
+        // Temperature sensor
+        if (options.temperature) {
+            let temperature = new Service.TemperatureSensor(options.temperature.name || 'Temperature');
 
-    // Rotation speed
-    this.service
-      .getCharacteristic(Characteristic.RotationSpeed)
-      .setProps({
-        minValue: 0, // 0 - turn off
-        maxValue: this.model === 'ca1' ? 4 : 3, // auto - for zhimi.humidifier.ca1 
-        minStep: 1,
-      })
-      .on('get', this.getRotationSpeed.bind(this))
-      .on('set', this.setRotationSpeed.bind(this))
+            temperature
+                .getCharacteristic(Characteristic.CurrentTemperature)
+                .on('get', this.getCurrentTemperature.bind(this));
 
-    // Create service info
-    this.serviceInfo = new Service.AccessoryInformation()
-
-    this.serviceInfo
-      .setCharacteristic(Characteristic.Manufacturer, 'Xiaomi')
-      .setCharacteristic(Characteristic.Model, 'Humidifier')
-      .setCharacteristic(Characteristic.SerialNumber, 'Undefined')
-
-    this.services.push(this.service)
-    this.services.push(this.serviceInfo)
-
-    // Temperature
-    if (this.showTemperature) {
-      this.temperatureSensorService = new Service.TemperatureSensor(this.nameTemperature);
-
-      this.temperatureSensorService
-        .getCharacteristic(Characteristic.CurrentTemperature)
-        .on('get', this.getCurrentTemperature.bind(this));
-
-      this.services.push(this.temperatureSensorService);
-    }
-
-    //Humidity
-    if (this.showHumidity){
-      this.humiditySensorService = new Service.HumiditySensor(this.nameHumidity);
-      this.humiditySensorService
-        .getCharacteristic(Characteristic.CurrentRelativeHumidity)
-        .on('get', this.getCurrentRelativeHumidity.bind(this));
-      
-      this.services.push(this.humiditySensorService);
-    }
-    
-
-    this.discover()
-  }
-
-  getServices() {
-    return this.services
-  }
-
-  async discover() {
-    try {
-      this.device = await miio.device({ address: this.ip, token: this.token })
-    } catch (e) {
-      this.log.error('Fail to discover the device. Retry in 1 minute', e)
-      setTimeout(() => { this.discover() }, 60000)
-    }
-  }
-
-  async getActive(callback) {
-    try {
-      const [ power ] = await this.device.call('get_prop', ['power'])
-
-      callback(
-        null,
-        (power === 'on')
-          ? Characteristic.Active.ACTIVE
-          : Characteristic.Active.INACTIVE
-      )
-    } catch (e) {
-      this.log.error('getActive', e)
-      callback(e)
-    }
-  }
-
-  async setActive(state, callback) {
-    try {
-      const power = (state === Characteristic.Active.ACTIVE)
-        ? 'on'
-        : 'off'
-
-      const [ result ] = await this.device.call('set_power', [power])
-
-      if (result !== 'ok')
-        throw new Error(result)
-
-      callback()
-    } catch (e) {
-      this.log.error('setActive', e)
-      callback(e)
-    }
-  }
-
-  async getCurrentHumidifierState(callback) {
-    try {
-      const [ power ] = await this.device.call('get_prop', ['power'])
-      callback(
-        null,
-        (power === 'on')
-          ? Characteristic.CurrentHumidifierDehumidifierState.HUMIDIFYING
-          : Characteristic.CurrentHumidifierDehumidifierState.INACTIVE
-      )
-    } catch (e) {
-      this.log.error('getCurrentHumidifierState', e)
-      callback(e)
-    }
-  }
-
-  async getCurrentRelativeHumidity(callback) {
-    try {
-      const [ humidity ] = await this.device.call('get_prop', ['humidity'])
-      callback(null, humidity)
-    } catch (e) {
-      this.log.error('getCurrentRelativeHumidity', e)
-      callback(e)
-    }
-  }
-
-  async getTargetRelativeHumidity(callback) {
-    try {
-      const [ limit_hum ] = await this.device.call('get_prop', ['limit_hum'])
-      callback(null, limit_hum)
-    } catch (e) {
-      this.log.error('getTargetRelativeHumidity', e)
-      callback(e)
-    }
-  }
-
-  async setTargetRelativeHumidity(value, callback) {
-    try {
-      const [ result ] = await this.device.call('set_limit_hum', [value])
-
-      if (result !== 'ok')
-        throw new Error(result)
-
-      callback()
-    } catch (e) {
-      this.log.error('setTargetRelativeHumidity', e)
-      callback(e)
-    }
-  }
-
-  async getWaterLevel(callback) {
-    try {
-      const [ waterLevel ] = await this.device.call('get_prop', ['depth'])
-      callback(null, waterLevel / 1.2)
-    } catch (e) {
-      this.log.error('getWaterLevel', e)
-      callback(e)
-    }
-  }
-
-  async getRotationSpeed(callback) {
-    try {
-      const modeToSpeed = {
-        'auto':   4,
-        'silent': 1,
-        'medium': 2,
-        'high':   3,
-      }
-
-      const mode = await this.device.call('get_prop', ['mode'])
-
-      callback(null, modeToSpeed[mode])
-    } catch (e) {
-      this.log.error('getRotationSpeed', e)
-      callback(e)
-    }
-  }
-
-  async setRotationSpeed(value, callback) {
-    try {
-      const speedToMode = {
-        1: 'silent',
-        2: 'medium',
-        3: 'high',
-      }
-
-      let result
-      const [ power ] = await this.device.call('get_prop', ['power'])
-      if (value > 0) {
-        if (power === 'off') {
-          await this.device.call('set_power', ['on'])
+            this.services.push(temperature);
         }
-        if (value < 4) {
-          [ result ] = await this.device.call('set_mode', [speedToMode[value]])
+
+        // Humidity sensor
+        if (options.humidity){
+            let humidity = new Service.HumiditySensor(options.humidity.name || 'Humidity');
+
+            humidity
+                .getCharacteristic(Characteristic.CurrentRelativeHumidity)
+                .on('get', this.getCurrentRelativeHumidity.bind(this));
+
+            this.services.push(humidity);
         }
-        if (value === 4 && this.model === 'ca1') {
-          [ result ] = await this.device.call('set_mode', ['auto'])
+
+        void this.discover();
+    }
+
+    getServices() {
+        return this.services;
+    }
+
+    async discover() {
+        try {
+            this.device = await miio.device({ address: this.ip, token: this.token });
+        } catch (err) {
+            this.log.error('Fail to discover the device. Retry in 1 minute\n', err);
+            setTimeout(() => { this.discover(); }, 60000);
         }
-      } else {
-         [ result ] = await this.device.call('set_power', ['off'])
-      }
-
-      if (result !== 'ok')
-        throw new Error(result)
-
-      callback()
-    } catch (e) {
-      this.log.error('setRotationSpeed', e)
-      callback(e)
     }
-  }
 
-  async getCurrentTemperature(callback) {
-    try {
-      const [ temperature ] = await this.device.call('get_prop', ['temp_dec'])
-      callback(null, temperature / 10)
-    } catch (e) {
-      this.log.error('getCurrentTemperature', e)
-      callback(e)
+    async getActive(callback) {
+        try {
+            const [power] = await this.device.call('get_prop', ['power']),
+                state = power === 'on' ? Characteristic.Active.ACTIVE : Characteristic.Active.INACTIVE;
+
+            callback(null, state);
+        } catch (err) {
+            this.log.error('getActive', err);
+            callback(err);
+        }
     }
-  }
+
+    async setActive(state, callback) {
+        try {
+            const power = state === Characteristic.Active.ACTIVE ? 'on' : 'off',
+                [result] = await this.device.call('set_power', [power]);
+
+            if (result !== 'ok') throw new Error(result);
+
+            callback();
+        } catch (err) {
+            this.log.error('setActive', err);
+            callback(err);
+        }
+    }
+
+    async getCurrentHumidifierState(callback) {
+        try {
+            const [power] = await this.device.call('get_prop', ['power']),
+                state = power === 'on' ?
+                    Characteristic.CurrentHumidifierDehumidifierState.HUMIDIFYING :
+                    Characteristic.CurrentHumidifierDehumidifierState.INACTIVE;
+
+            callback(null, state);
+        } catch (err) {
+            this.log.error('getCurrentHumidifierState', err);
+            callback(err);
+        }
+    }
+
+    async getCurrentRelativeHumidity(callback) {
+        try {
+            const [humidity] = await this.device.call('get_prop', ['humidity']);
+
+            callback(null, humidity);
+        } catch (err) {
+            this.log.error('getCurrentRelativeHumidity', err);
+            callback(err);
+        }
+    }
+
+    async getTargetRelativeHumidity(callback) {
+        try {
+            const [limit_hum] = await this.device.call('get_prop', ['limit_hum']);
+
+            callback(null, limit_hum);
+        } catch (err) {
+            this.log.error('getTargetRelativeHumidity', err);
+            callback(err);
+        }
+    }
+
+    async setTargetRelativeHumidity(value, callback) {
+        try {
+            const [result] = await this.device.call('set_limit_hum', [value]);
+
+            if (result !== 'ok') throw new Error(result);
+
+            callback();
+        } catch (err) {
+            this.log.error('setTargetRelativeHumidity', err);
+            callback(err);
+        }
+    }
+
+    async getWaterLevel(callback) {
+        try {
+            const [waterLevel] = await this.device.call('get_prop', ['depth']);
+
+            // TODO: magic number
+            callback(null, waterLevel / 1.2);
+        } catch (err) {
+            this.log.error('getWaterLevel', err);
+            callback(err);
+        }
+    }
+
+    async getRotationSpeed(callback) {
+        try {
+            const mode = await this.device.call('get_prop', ['mode']),
+                speed = speedLevels.findIndex(item => item === mode);
+
+            callback(null, speed);
+        } catch (err) {
+            this.log.error('getRotationSpeed', err);
+            callback(err);
+        }
+    }
+
+    async setRotationSpeed(value, callback) {
+        try {
+            const [power] = await this.device.call('get_prop', ['power']);
+
+            let result;
+
+            if (value > 0){
+                power === 'off' && await this.device.call('set_power', ['on']);
+                [result] = await this.device.call('set_mode', [speedLevels[value]]);
+            } else {
+                [result] = await this.device.call('set_power', ['off'])
+            }
+
+            if (result !== 'ok') throw new Error(result);
+
+            callback()
+        } catch (err) {
+            this.log.error('setRotationSpeed', err);
+            callback(err);
+        }
+    }
+
+    async getCurrentTemperature(callback) {
+        try {
+            const [temperature] = await this.device.call('get_prop', ['temp_dec']);
+
+            // TODO: magic number
+            callback(null, temperature / 10);
+        } catch (err) {
+            this.log.error('getCurrentTemperature', err);
+            callback(err);
+        }
+    }
 }
