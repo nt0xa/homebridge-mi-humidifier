627a6637df4410c5ee67c4d7d40c6cb738e766d2
PietroLubini <lubinipietro@gmail.com>
2020-04-26 20:19:20 +0300

    Refactor: get rid of code duplications

--- devices/constants.js
+++ devices/constants.js
@@ -1,3 +1,9 @@
+const CharacteristicOperation = {
+    GET: 1,
+    SET: 2
+};
+
 module.exports = Object.freeze({
-    MODES: ['off', 'silent', 'medium', 'high', 'auto']
+    MODES: ['off', 'silent', 'medium', 'high', 'auto'],
+    CharacteristicOperation
 });
